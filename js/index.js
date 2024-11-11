ba5b6bfeb9558c35d9ef20261dbf8fc1cf744584
3bdulrahman-elgaly <algalycom30@gmail.com>
2024-11-11 03:50:45 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,45 @@
+// function calPrice(price, profit, tax , ads) {
+//     var price1=price+ profit;
+//     var price2=price1+tax;
+//     var price3=price2+ads;
+//     return price3;
+// }
+// var res= calPrice(900, 100,1.2,100)
+
+// function getAvg(num1 ,num2){
+//     var sum= num1 +num2;
+//     var avg=sum/2;
+//     console.log(avg);
+//     return avg ;
+
+    
+// }
+// getAvg(res ,900)
+
+
+
+
+
+
+
+
+
+
+// function display(count, content, elmId ) {
+//     var cartona=''
+
+//     for (var i = 0; i <count; i++) {
+//     cartona +='<h2>' +content+'</h2>'
+
+        
+//     }
+//     document.getElementById(elmId).innerHTML=cartona;
+// }
+// display(100,'Home', 'home');
+// display(100,'About', 'about');
+// display(100,'Contact', 'contact');
+
+
+function hello() {
+    document.getElementById('demo').innerHTML+='<img src="./images/two.png" alt="">'
+}
